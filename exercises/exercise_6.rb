1e169d87b7978955cb5d9e7bf6f1b2da535f6d57
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-07-24 15:08:00 -0600

    Create employees for store 1 and 2 doing one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,29 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "William", last_name: "Taylor", hourly_rate: 75)
+@store1.employees.create(first_name: "Elizabeth", last_name: "Anderson", hourly_rate: 80)
+@store1.employees.create(first_name: "Jennifer", last_name: "Thomas", hourly_rate: 85)
+@store1.employees.create(first_name: "Charles", last_name: "Jackson", hourly_rate: 90)
+@store1.employees.create(first_name: "Margaret", last_name: "White", hourly_rate: 95)
+@store1.employees.create(first_name: "Joseph", last_name: "Harris", hourly_rate: 100)
+@store1.employees.create(first_name: "Susan", last_name: "Martin", hourly_rate: 70)
+@store1.employees.create(first_name: "Jessica", last_name: "Thompson", hourly_rate: 65)
+@store1.employees.create(first_name: "Sarah", last_name: "Garcia", hourly_rate: 60)
+@store1.employees.create(first_name: "Mohammed", last_name: "Martinez", hourly_rate: 55)
+
+
+
+
+@store2.employees.create(first_name: "David", last_name: "Robinson", hourly_rate: 80)
+@store2.employees.create(first_name: "Mary", last_name: "Clark", hourly_rate: 85)
+@store2.employees.create(first_name: "Christopher", last_name: "Rodriguez", hourly_rate: 90)
+@store2.employees.create(first_name: "Daniel", last_name: "Lewis", hourly_rate: 95)
+@store2.employees.create(first_name: "Patricia", last_name: "Lee", hourly_rate: 100)
+@store2.employees.create(first_name: "Richard", last_name: "Walker", hourly_rate: 75)
+@store2.employees.create(first_name: "Linda", last_name: "Hall", hourly_rate: 70)
+@store2.employees.create(first_name: "Robert", last_name: "Allen", hourly_rate: 65)
+@store2.employees.create(first_name: "Thomas", last_name: "Young", hourly_rate: 60)
+@store2.employees.create(first_name: "Susan", last_name: "Hernandez", hourly_rate: 55)

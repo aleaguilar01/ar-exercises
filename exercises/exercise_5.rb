655ada88448c3e5766901dfcc922a4c087501286
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-07-24 14:46:31 -0600

    Add calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+Store.find_each  do |store|
+  puts "#{store.name} annual revenue is #{store.annual_revenue}"
+end
+
+
+puts Store.where("annual_revenue > ?", 1000000).count
